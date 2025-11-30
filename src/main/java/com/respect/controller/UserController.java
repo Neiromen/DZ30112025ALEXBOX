b97d4de9f223ca366050d0a2c497eee65bfa0899
Neiromen <alex.kor0702@gmail.com>
2025-12-01 00:38:07 +0300

    curls added & remove useless entity functional

--- src/main/java/com/respect/controller/UserController.java
+++ src/main/java/com/respect/controller/UserController.java
@@ -13,31 +13,46 @@ public class UserController {
 
     private final List<User> userList = new ArrayList<>();
 
+    //curl --location 'localhost:8081/api/user' \
+    //--header 'Content-Type: application/json' \
+    //--data '{
+    //    "name":"Skipov",
+    //    "age":10
+    //}'
     @PostMapping("/user")
-    public ResponseEntity<List<User>> addUser(@RequestBody User user){
+    public ResponseEntity<List<User>> addUser(@RequestBody User user) {
         userList.add(user);
         return ResponseEntity.ok(userList);
     }
 
+    //curl --location --request DELETE 'localhost:8081/api/user/0'
     @DeleteMapping("/user/{index}")
-    public ResponseEntity<Void> deleteById(@PathVariable int index){
+    public ResponseEntity<Void> deleteById(@PathVariable int index) {
         userList.remove(index);
         return ResponseEntity.noContent().build();
     }
 
+    //curl --location 'localhost:8081/api/user/0'
     @GetMapping("/user/{index}")
-    public ResponseEntity<User> getUser(@PathVariable int index){
+    public ResponseEntity<User> getUser(@PathVariable int index) {
         return ResponseEntity.ok(userList.get(index));
     }
 
+    //curl --location 'localhost:8081/api/users'
     @GetMapping("/users")
-    public ResponseEntity<List<User>> getUsers(){
+    public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok(userList);
     }
 
+    //curl --location --request PUT 'localhost:8081/api/user/0' \
+    //--header 'Content-Type: application/json' \
+    //--data '{
+    //    "name":"Skipov",
+    //    "age":11
+    //}'
     @PutMapping("/user/{index}")
-    public ResponseEntity<User> updateUser(@PathVariable int index,@RequestBody User user){
-        userList.set(index,user);
+    public ResponseEntity<User> updateUser(@PathVariable int index, @RequestBody User user) {
+        userList.set(index, user);
         return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/respect/entity/User.java
+++ src/main/java/com/respect/entity/User.java
@@ -4,14 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Entity(name = "users")
 @Data
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
-    private int id;
-
     @Column
     private String name;
 
